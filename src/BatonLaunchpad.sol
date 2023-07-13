28681227b4e3eda5ed64e0a82a0ea0fde5d55f65
memtick <tajikistanismylove@gmail.com>
2023-07-13 19:41:01 +0100

    add libclone

--- src/BatonLaunchpad.sol
+++ src/BatonLaunchpad.sol
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {LibClone} from "solady/utils/LibClone.sol";
 import {Nft} from "./Nft.sol";
 
 contract BatonLaunchpad {
-    function create(string calldata name, string calldata symbol, Nft.Category[] calldata categories) public {
-        new Nft(name, symbol, categories);
+    using LibClone for address;
+
+    address public nftImplementation;
+
+    constructor(address _nftImplementation) {
+        setNftImplementation(_nftImplementation);
+    }
+
+    function create(string calldata name, string calldata symbol, Nft.Category[] calldata categories, bytes32 salt)
+        public
+        returns (Nft nft)
+    {
+        nft = Nft(payable(nftImplementation.cloneDeterministic(salt)));
+    }
+
+    function setNftImplementation(address _nftImplementation) public {
+        nftImplementation = _nftImplementation;
     }
 }
