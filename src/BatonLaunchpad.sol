a781da72ee8caed6abd18052b07a1be5ec421415
memtick <tajikistanismylove@gmail.com>
2023-07-13 19:41:01 +0100

    add baton create

--- src/BatonLaunchpad.sol
+++ src/BatonLaunchpad.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract BatonLaunchpad {}
+import {Nft} from "./Nft.sol";
+
+contract BatonLaunchpad {
+    function create(string calldata name, string calldata symbol, Nft.Category[] calldata categories) public {
+        new Nft(name, symbol, categories);
+    }
+}
